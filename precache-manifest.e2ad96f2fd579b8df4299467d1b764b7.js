286df212ba425d72bbfd6b7e61e7f2dc3797993e
Sukriti Bhardwaj <sukriti@lawn-128-61-48-150.lawn.gatech.edu>
2020-06-09 16:13:49 -0400

    Updates

---
+++ precache-manifest.e2ad96f2fd579b8df4299467d1b764b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9ef112fabdbfa3e98b99e15d388b85",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ceec1533f5b1bdb4f9c7",
+    "url": "./static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "172243aac6f3e2067e32",
+    "url": "./static/css/main.2b29259e.chunk.css"
+  },
+  {
+    "revision": "ceec1533f5b1bdb4f9c7",
+    "url": "./static/js/2.b0e192fb.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "./static/js/2.b0e192fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172243aac6f3e2067e32",
+    "url": "./static/js/main.60bc0468.chunk.js"
+  },
+  {
+    "revision": "4bc7c495abcd4eec1182",
+    "url": "./static/js/runtime-main.9e83bf12.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personalWebsite/precache-manifest.2ac9c60f013bcc7fa01bb6168cd26b00.js"
+  "./precache-manifest.e2ad96f2fd579b8df4299467d1b764b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personalWebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
